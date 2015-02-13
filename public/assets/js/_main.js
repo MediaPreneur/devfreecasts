8f4289d68589f81986c0d23403cf86a5ceaa3dc3
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2015-02-13 19:11:28 +0000

    rollback to static render

--- public/assets/js/_main.js
+++ public/assets/js/_main.js
@@ -1,77 +1,21 @@
 (function() {
-  var toggle = document.querySelector(".navbar-toggle")
-    , collapse = document.querySelector(".navbar-collapse")
-    , dfcURL = document.querySelector("[data-dfc-url]")
-    , dfcTemplate = document.querySelector("[data-dfc-template]")
-    , filterCategory = document.querySelectorAll("[data-dfc-category]")
-  ;
 
-  toggle.addEventListener("click", function() {
-    collapse.classList.toggle("hidden-xs");
+  window.addEventListener("load", function() {
+    var toggle = document.querySelector(".navbar-toggle")
+      , collapse = document.querySelector(".navbar-collapse")
+    ;
+
+    toggle.addEventListener("click", function() {
+      collapse.classList.toggle("hidden-xs");
+    });
+
+    if (window.location.hash) {
+      var anchor = document.querySelector(window.location.hash);
+      anchor.nextElementSibling.classList.add("thumbnail-featured");
+    }
+
+    asyncImg();
+
   });
   
-  for (var i = 0, len = filterCategory.length || 0; i < len; i++) {
-    filterCategory[i].addEventListener("click", function(e) {
-      var data = JSON.parse(localStorage.getItem('devfreecasts'))
-        , category = e.target.getAttribute("data-dfc-category")
-        , template = dfcTemplate.getAttribute("data-dfc-template")
-        , dfcType = e.target.getAttribute("data-dfc")
-        , hasItems = true
-      ;
-      if (dfcType === "video") {
-        if (category) {
-          var items = 0;
-          data.partners.forEach(function(partner) {
-            partner.videos = partner.videos.filter(function(video) {
-              return video.level == category;
-            });
-            items += partner.videos.length;
-          });
-          hasItems = items > 0;
-        }
-      } else if (dfcType === "platform") {
-        if (category) {
-          data.platforms = data.platforms.filter(function(platform) {
-            return platform.category == category;
-          });
-          hasItems = data.platforms.length > 0;
-        }
-      }
-      if (hasItems) {
-        dfcTemplate.innerHTML = DFC[template](data);
-      } else {
-        dfcTemplate.innerHTML = "<div class='result-status'><h3>Nothing was found here :(</h3></div>";
-      }
-    });
-  }
-
-  var shuffle = function(collection) {
-    collection && collection.sort(function() { return Math.random() - 0.5; });
-  };
-
-  var renderTemplate = function() {
-    if (dfcURL && dfcTemplate) {
-      var url = dfcURL.getAttribute("data-dfc-url")
-        , template = dfcTemplate.getAttribute("data-dfc-template")
-      ;
-      request = new XMLHttpRequest();
-      request.open('GET', url, true);
-
-      request.onload = function() {
-        var data = JSON.parse(request.responseText);
-        data.platforms && shuffle(data.platforms);
-        dfcTemplate.innerHTML = DFC[template](data);
-        localStorage.setItem('devfreecasts', request.responseText);
-      };
-      request.send();
-    }
-  };
-
-  renderTemplate();
-
-  // For Firefox OS Hosted app
-  if (!!(navigator.mozApps && navigator.mozApps.installPackage)) {
-    navigator.mozApps.installPackage(window.location.origin + "/devfreecasts/manifest.webapp");
-  }
-
 })();
