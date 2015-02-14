0cdfa2312ec105963cf36f2e87f18ccd1e65b40a
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2015-02-14 20:16:14 +0000

    fix feed generator

--- feed.js
+++ feed.js
@@ -4,39 +4,51 @@ module.exports = function() {
     , RSS = require("rss")
     , moment = require("moment")
     , fs = require("fs")
-    , root_url = harp.root_url[process.env.NODE_ENV]
-    , image_root_url = root_url + "assets/images/"
-    , output_feed = "public/rss.xml"
+    , rootUrl = harp.root_url[process.env.NODE_ENV]
+    , imageRootUrl = rootUrl + "assets/images/"
+    , outputFeed = "public/rss.xml"
+    , videos = []
   ;
 
   var feed = new RSS({
     title: harp.title,
     description: harp.description,
-    feed_url: root_url + "rss.xml",
-    site_url: root_url,
-    image_url: image_root_url + "devfreecasts-128.png",
+    feed_url: rootUrl + "rss.xml",
+    site_url: rootUrl,
+    image_url: imageRootUrl + "devfreecasts-128.png",
     managingEditor: harp.author,
     pubDate: moment().format("LLLL"),
     ttl: 60
   });
   
-  harp.platforms.forEach(function(platform_name) {
-    var platform = require("./public/"+ platform_name +"/_data.json")["index"];
+  harp.platforms.forEach(function(platformName) {
+    var platform = require("./public/"+ platformName +"/_data.json")["index"];
 
     platform.videos.forEach(function(video) {
-      var video_id = video.title.replace(/[^\w\s]/g, "").replace(/\s/g, "-").toLowerCase();
-      var video_link = root_url + platform_name + "/#" + video_id;
-      var video_published_at = moment(video.published_at || moment().format("YYYYMMDD"), "YYYYMMDD");
+      var videoId = video.title.replace(/[^\w\s]/g, "").replace(/\s/g, "-").toLowerCase();
+      var videoLink = rootUrl + platformName + "/#" + videoId;
+      var videoPublishedAt = moment(video.published_at || moment().format("YYYYMMDD"), "YYYYMMDD");
 
-      feed.item({
+      videos.push({
         title: "Video: " + video.title,
         description: video.description,
-        url: video_link,
+        url: videoLink,
         author: harp.author,
-        date: video_published_at.format("ll")
+        date: videoPublishedAt.format("ll")
       });
     });
   });
-  fs.writeFileSync(output_feed, feed.xml());
-  console.log("Generated RSS:", output_feed);
+
+  var sortedVideos = videos.sort(function(a, b) {
+    var a = moment(a.date, "ll").toDate().getTime();
+    var b = moment(b.date, "ll").toDate().getTime();
+    if (a < b) return 1;
+    if (a > b) return -1;
+    return 0;
+  });
+
+  sortedVideos.forEach(function(video) { feed.item(video); });
+
+  fs.writeFileSync(outputFeed, feed.xml());
+  console.log("Generated RSS:", outputFeed);
 };
